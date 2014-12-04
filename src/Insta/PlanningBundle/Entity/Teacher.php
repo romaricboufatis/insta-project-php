bd974dfc8d94365ba97c19e718be0e623cfc3224
Administrator <michael.soares.75@gmail.com>
2014-12-04 11:59:00 +0100

    Ajout des fichiers Site.php et Room.php

--- src/Insta/PlanningBundle/Entity/Teacher.php
+++ src/Insta/PlanningBundle/Entity/Teacher.php
@@ -3,6 +3,7 @@
 namespace Insta\PlanningBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\GroupInterface;
 
 /**
  * Teacher
@@ -12,4 +13,64 @@ use Doctrine\ORM\Mapping as ORM;
 class Teacher extends User
 {
 
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $groups;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->groups = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add groups
+     *
+     * @param \Insta\PlanningBundle\Entity\Group $groups
+     * @return Teacher
+     */
+    public function addGroup(GroupInterface $groups)
+    {
+        $this->groups[] = $groups;
+
+        return $this;
+    }
+
+    /**
+     * Remove groups
+     *
+     * @param \Insta\PlanningBundle\Entity\Group $groups
+     */
+    public function removeGroup(GroupInterface $groups)
+    {
+        $this->groups->removeElement($groups);
+    }
+
+    /**
+     * Get groups
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getGroups()
+    {
+        return $this->groups;
+    }
 }
